af413244d225618c1737667b1f915e0ee2677342
emelyumer <emi_92rs@abv.bg>
2023-04-28 19:17:11 +0300

    initial commit

--- Python Advanced - may 2023/01_Lists_as_stacks_and_queues/1_reverse_strings.py
+++ Python Advanced - may 2023/01_Lists_as_stacks_and_queues/1_reverse_strings.py
@@ -1,3 +1,12 @@
-some_text = input()
-reversed_text = some_text[::-1]
-print(reversed_text)
+# first way
+# some_text = input()
+# reversed_text = some_text[::-1]
+# print(reversed_text)
+
+# second way
+new_text = []
+some_text = list(input())
+for index in range(len(some_text)):
+    symbol = some_text.pop()
+    new_text.append(symbol)
+print(''.join(new_text))
