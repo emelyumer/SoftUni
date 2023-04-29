53ce4613e741008f3cc7d9f38f016d1fd94e27c6
emelyumer <emi_92rs@abv.bg>
2023-04-29 10:38:03 +0300

    initial commit

--- Python Advanced - may 2023/01_Lists_as_stacks_and_queues/3_supermarket.py
+++ Python Advanced - may 2023/01_Lists_as_stacks_and_queues/3_supermarket.py
@@ -1,12 +1,23 @@
+# command = input()
+# client_list = []
+# while command != "End":
+#     if command == "Paid":
+#         for name in client_list:
+#             print(name)
+#         client_list.clear()
+#     else:
+#         client_list.append(command)
+#     command = input()
+# print(f"{len(client_list)} people remaining.")
+# second way
+from collections import deque
 command = input()
-client_list = []
+client_list = deque()
 while command != "End":
     if command == "Paid":
-        for name in client_list:
-            print(name)
-        client_list.clear()
+        while client_list:
+            print(client_list.popleft())
     else:
         client_list.append(command)
     command = input()
 print(f"{len(client_list)} people remaining.")
-
