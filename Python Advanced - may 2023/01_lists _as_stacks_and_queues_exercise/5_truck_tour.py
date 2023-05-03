2654eb69c7a72c393f99201ce178a1d1118ced05
emelyumer <emi_92rs@abv.bg>
2023-05-03 07:21:33 +0300

    exam

---
+++ Python Advanced - may 2023/01_lists _as_stacks_and_queues_exercise/5_truck_tour.py
@@ -0,0 +1,6 @@
+number_of_petrol_pumps = int(input())
+tank = 0
+for index in range(number_of_petrol_pumps):
+    petrol_pump = input().split()
+    lr = int(petrol_pump[0])
+    km = int(petrol_pump[1])
