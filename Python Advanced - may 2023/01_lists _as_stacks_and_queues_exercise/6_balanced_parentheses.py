60115234706aecf734d54479fac38c615d151b2a
emelyumer <emi_92rs@abv.bg>
2023-05-04 06:18:52 +0300

    exam

--- Python Advanced - may 2023/01_lists _as_stacks_and_queues_exercise/6_balanced_parentheses.py
+++ Python Advanced - may 2023/01_lists _as_stacks_and_queues_exercise/6_balanced_parentheses.py
@@ -1,11 +1,17 @@
 from collections import deque
-all_parentheses = deque(input().split())
+all_parentheses = deque(input())
 left_parentheses = deque()
+all_combination = ["{}", "()", "[]"]
 for par in all_parentheses:
     if par in "{[(":
         left_parentheses.append(par)
     elif not left_parentheses:
         print("NO")
+        break
     else:
         if left_parentheses:
-            if par + left_parentheses
+            if left_parentheses.pop() + par not in all_combination:
+                print("NO")
+                break
+else:
+    print("YES")
