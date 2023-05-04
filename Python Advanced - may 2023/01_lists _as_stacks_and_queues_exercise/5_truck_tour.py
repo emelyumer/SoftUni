16822e7da8b9270be0391474172231ca28c86fbb
emelyumer <emi_92rs@abv.bg>
2023-05-04 05:31:19 +0300

    exam

--- Python Advanced - may 2023/01_lists _as_stacks_and_queues_exercise/5_truck_tour.py
+++ Python Advanced - may 2023/01_lists _as_stacks_and_queues_exercise/5_truck_tour.py
@@ -1,6 +1,25 @@
+from collections import deque
 number_of_petrol_pumps = int(input())
+petrol_pump_list = deque(list())
+
 tank = 0
-for index in range(number_of_petrol_pumps):
+index = 0
+for idx in range(number_of_petrol_pumps):
     petrol_pump = input().split()
-    lr = int(petrol_pump[0])
-    km = int(petrol_pump[1])
+    petrol_pump_list.append(petrol_pump)
+
+used_list = petrol_pump_list.copy()
+
+while used_list:
+    lr = int(used_list[0][0])
+    km = int(used_list[0][1])
+    tank += lr
+    if tank >= km:
+        tank -= km
+        used_list.popleft()
+    else:
+        petrol_pump_list.rotate(-1)
+        used_list = petrol_pump_list.copy()
+        index += 1
+        tank = 0
+print(index)
