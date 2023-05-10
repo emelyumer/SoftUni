bb324b13f6032a59ea2e4e7acb94a4c6f980e276
emelyumer <emi_92rs@abv.bg>
2023-05-10 23:39:37 +0300

    commit

--- Python Advanced - may 2023/02_tuples_an_sets_exercise/1_unique_usernames.py
+++ Python Advanced - may 2023/02_tuples_an_sets_exercise/1_unique_usernames.py
@@ -1,12 +1,5 @@
-# n_lines = int(input())
-# unique_usernames = set()
-# for _ in range(n_lines):
-#     username = input()
-#     unique_usernames.add(username)
-# print('\n'.join(unique_usernames))
-
 n_lines = int(input())
-unique_usernames = {}
+unique_usernames = set()
 for _ in range(n_lines):
     username = input()
     unique_usernames.add(username)
