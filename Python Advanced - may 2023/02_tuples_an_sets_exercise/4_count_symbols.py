fd4e702704b6791d4ef23577430416b38823074b
emelyumer <emi_92rs@abv.bg>
2023-05-13 02:57:21 +0300

    commit

--- Python Advanced - may 2023/02_tuples_an_sets_exercise/4_count_symbols.py
+++ Python Advanced - may 2023/02_tuples_an_sets_exercise/4_count_symbols.py
@@ -1,4 +1,4 @@
 symbols = input()
 counted_symbols = {sym:symbols.count(sym) for sym in symbols}
-for k,v in counted_symbols.items():
+for k,v in sorted(counted_symbols.items()):
     print(f"{k}:{v} time/s")
