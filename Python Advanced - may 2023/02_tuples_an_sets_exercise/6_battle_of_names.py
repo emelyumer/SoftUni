7bd53f61c88884a127bf635fbed7f7adaddd342d
emelyumer <emi_92rs@abv.bg>
2023-05-15 23:47:29 +0300

    commit

--- Python Advanced - may 2023/02_tuples_an_sets_exercise/6_battle_of_names.py
+++ Python Advanced - may 2023/02_tuples_an_sets_exercise/6_battle_of_names.py
@@ -1,2 +1,15 @@
-for names in range(int(input())):
+odd_set = set()
+even_set = set()
+
+for row in range(1, int(input()) + 1):
     name = input()
+    ascii_sum_name = sum(ord(letter) for letter in name) // row
+
+    if ascii_sum_name % 2 == 0:
+        even_set.add(ascii_sum_name)
+    else:
+        odd_set.add(ascii_sum_name)
+
+
+
+
