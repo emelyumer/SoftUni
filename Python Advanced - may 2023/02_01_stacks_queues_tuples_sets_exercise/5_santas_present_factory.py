884c4ae4dc880d782188896ee29375323a1b0fcb
emelyumer <emi_92rs@abv.bg>
2023-05-23 00:29:44 +0300

    commit

--- Python Advanced - may 2023/02_01_stacks_queues_tuples_sets_exercise/5_santas_present_factory.py
+++ Python Advanced - may 2023/02_01_stacks_queues_tuples_sets_exercise/5_santas_present_factory.py
@@ -1,8 +1,8 @@
 from collections import deque
 materials_toys = deque(int(x) for x in input().split())
 magic_level = deque(int(x) for x in input().split())
-gift_level = {150: ["Doll", 0], 250: ["Wooden train", 0], 300: ["Teddy bear", 0], 400: ["Bicycle", 0]}
-# presents = {"Doll": 0, "Wooden train": 0, "Teddy bear": 0, "Bicycle": 0}
+gift_level = {150: "Doll", 250: "Wooden train", 300: "Teddy bear", 400: "Bicycle"}
+presents = {"Bicycle":0, "Doll":0, "Wooden train":0, "Teddy bear":0}
 no_presents = True
 
 while materials_toys and magic_level:
@@ -12,20 +12,20 @@ while materials_toys and magic_level:
     if material == 0 and magic == 0:
         continue
     elif material != 0 and magic == 0:
-        materials_toys.append(material + 15)
+        materials_toys.append(material)
     elif material ==0 and magic != 0:
         magic_level.appendleft(magic)
     elif result in gift_level.keys():
-        gift_level[result][1] += 1
+        presents[gift_level[result]] += 1
     elif result < 0:
         result = material + magic
         materials_toys.append(result)
     elif result not in gift_level.keys():
         materials_toys.append(material + 15)
 
-if gift_level[150][1] > 0 and gift_level[250][1] > 0:
+if presents["Bicycle"] > 0 and presents["Teddy bear"] > 0:
     no_presents = False
-elif gift_level[300][1] > 0 and gift_level[400][1] > 0:
+elif presents["Doll"] > 0 and presents["Wooden train"] > 0:
     no_presents = False
 
 if no_presents:
@@ -34,11 +34,10 @@ else:
     print("The presents are crafted! Merry Christmas!")
 
 if materials_toys:
-    print(f"Materials left: {', '.join(str(x) for x in materials_toys)}")
+    print(f"Materials left: {', '.join([str(x) for x in materials_toys][::-1])}")
 if magic_level:
     print(f"Magic left: {', '.join(str(x) for x in magic_level)}")
 
-
-
-
-
+for key, value in presents.items():
+    if value > 0:
+        print(f"{key}: {value}")
