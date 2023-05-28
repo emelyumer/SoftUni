df3572b5d19208320ecd3cf19ba1eedf5c0dc8ba
emelyumer <emi_92rs@abv.bg>
2023-05-28 04:01:35 +0300

    commit

---
+++ Python Advanced - may 2023/03_multidimensional_lists_exercise_1/5_matrix_of_palindromes.py
@@ -0,0 +1,8 @@
+rows, cols = [int(x) for x in input().split()]
+start_letter = ord("a")
+
+for row in range(start_letter, start_letter + rows):
+    for col in range(start_letter, start_letter + cols):
+        print(f"{chr(row)}{chr(row + col - start_letter)}{chr(row)}", end=" ")
+
+    print()
