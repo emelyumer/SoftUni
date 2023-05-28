2164b84e46ecfc1b975ea5480ef1290505b91415
emelyumer <emi_92rs@abv.bg>
2023-05-28 04:41:27 +0300

    commit

--- Python Advanced - may 2023/03_multidimensional_lists_exercise_1/5_matrix_of_palindromes.py
+++ Python Advanced - may 2023/03_multidimensional_lists_exercise_1/5_matrix_of_palindromes.py
@@ -1,8 +1,17 @@
+# rows, cols = [int(x) for x in input().split()]
+# start_letter = ord("a")
+#
+# for row in range(start_letter, start_letter + rows):
+#     for col in range(start_letter, start_letter + cols):
+#         print(f"{chr(row)}{chr(row + col - start_letter)}{chr(row)}", end=" ")
+#
+#     print()
+
 rows, cols = [int(x) for x in input().split()]
 start_letter = ord("a")
 
 for row in range(start_letter, start_letter + rows):
-    for col in range(start_letter, start_letter + cols):
-        print(f"{chr(row)}{chr(row + col - start_letter)}{chr(row)}", end=" ")
+    for col in range(row, row + cols):
+        print(f"{chr(row)}{chr(col)}{chr(row)}", end=" ")
 
     print()
