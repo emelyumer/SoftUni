298bafd64eb93e0a0c2e00f275161beac5963021
emelyumer <emi_92rs@abv.bg>
2023-05-30 06:08:48 +0300

    commit

--- Python Advanced - may 2023/03_multidimensional_lists_exercise_1/10_radioactive_mutate_vampire_bunnies.py
+++ Python Advanced - may 2023/03_multidimensional_lists_exercise_1/10_radioactive_mutate_vampire_bunnies.py
@@ -4,6 +4,46 @@ def find_player_position():
             return row, matrix[row].index("P")
 
 
+def check_valid_index(row, col, player=False):
+    global win
+    if 0 <= row < rows and 0 <= col < cols:
+        return True
+    if player:
+        win = True
+
+
+def show_result(status="won"):
+    [print(*row, sep="") for row in matrix]
+    print(f"{status}: {player_row} {player_col}")
+
+    raise SystemExit
+
+
+def bunnies_positions():
+    positions = []
+
+    for row in range(rows):
+        for col in range(cols):
+            if matrix[row][col] == "B":
+                positions.append([row, col])
+
+    return positions
+
+
+def check_player_alive(row, col):
+    if matrix[row][col] == "B":
+        show_result("dead")
+
+
+def bunnies_move(bunnies_pos):
+    for row, col in bunnies_pos:
+        for b_move in directions.values():
+            new_row, new_col = row + b_move[0], col + b_move[1]
+
+            if check_valid_index(new_row, new_col):
+                matrix[new_row][new_col] = "B"
+
+
 rows, cols = [int(x) for x in input().split()]
 matrix = [list(input()) for _ in range(rows)]
 commands = input()
@@ -17,3 +57,17 @@ directions = {
 
 player_row, player_col = find_player_position()
 matrix[player_row][player_col] = "."
+
+
+for command in commands:
+    player_movement_row, player_movement_col = player_row + directions[command][0], player_col + directions[command][1]
+
+    if check_valid_index(player_movement_row, player_movement_col, True):
+        player_row, player_col = player_movement_row, player_movement_col
+
+    bunnies_move(bunnies_positions())
+
+    if win:
+        show_result()
+
+    check_player_alive(player_row, player_col)
