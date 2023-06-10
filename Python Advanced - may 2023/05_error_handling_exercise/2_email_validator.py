d81aeb42379afdbfef17b378f9030c414ea71f10
emelyumer <emi_92rs@abv.bg>
2023-06-10 04:50:10 +0300

    commit

---
+++ Python Advanced - may 2023/05_error_handling_exercise/2_email_validator.py
@@ -0,0 +1,45 @@
+import re
+
+class NameTooShortError(Exception):
+    pass
+
+
+class MustContainAtSymbolError(Exception):
+    pass
+
+
+class InvalidDomainError(Exception):
+    pass
+
+
+class MoreThanOneAtSymbolError(Exception):
+    pass
+
+
+class InvalidNameError(Exception):
+    pass
+
+
+MIN_LENGHT = 4
+VALID_DOMAINS = [".com", ".bg", ".org", ".net"]
+
+pattern_name = r'[\w]+'
+pattern_domain = r'\.[a-z]+'
+
+email = input()
+
+while email != "End":
+    if email.count("@") > 1:
+        raise MoreThanOneAtSymbolError("Email shoul contain only one @")
+    if len(email.split("@")[0]) < MIN_LENGHT:
+        raise NameTooShortError("Name must be more than 4 characters")
+    if "@" not in email:
+        raise MustContainAtSymbolError("Email must contain @")
+    if re.findall(pattern_name, email)[0] != email.split("@")[0]:
+        raise InvalidNameError("Name can contain only letters, digits and underscores!")
+    if re.findall(pattern_domain, email)[-1] not in VALID_DOMAINS:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+
+    print("Email is valid")
+
+    email = input()
