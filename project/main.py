29897d67dc8282ed8b9fd6e3e0a986a771c9892c
emelyumer <emi_92rs@abv.bg>
2023-07-10 22:01:52 +0300

    commit

--- project/main.py
+++ project/main.py
@@ -6,8 +6,7 @@ from project.song import Song
 song = Song("Running in the 90s", 3.45, False)
 print(song.get_info())
 album = Album("Initial D", song)
-second_song = Song("Around the World", 2.34,
-False)
+second_song = Song("Around the World", 2.34, False)
 print(album.add_song(second_song))
 print(album.details())
 print(album.publish())
