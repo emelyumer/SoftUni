411b2e9c6fb8b62e4acefd398ecd326420e5c6d7
emelyumer <emi_92rs@abv.bg>
2023-07-17 23:54:46 +0300

    commit

---
+++ project/cheetah.py
@@ -0,0 +1,6 @@
+from project.animal import Animal
+
+
+class Cheetah(Animal):
+    def __init__(self, name: str, gender: str, age: int, money_for_care: int):
+        super().__init__(self, name: str, gender: str, age: int, money_for_care = 60)
