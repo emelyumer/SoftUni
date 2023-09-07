1e6028b75c2ecb9a01852f4883a0f424cbf0132b
emelyumer <emi_92rs@abv.bg>
2023-09-07 06:50:24 +0300

    commit

---
+++ Python OOP - june 2023/09_decorators_exercise/1_logged.py
@@ -0,0 +1,17 @@
+def logged(func):
+    def wrapper(*args):
+        return f"you called {func.__name__}" \
+               f"({', '.join(str(arg) for arg in args)})\n" \
+               f"it returned {func(*args)}"
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+print(func(4, 4, 4))
+
+@logged
+def sum_func(a, b):
+    return a + b
+print(sum_func(1, 4))
---
+++ Python OOP - june 2023/09_decorators_exercise/2_even_parameters.py
@@ -0,0 +1,16 @@
+def even_parameters(func):
+    def wrapper(*args):
+        for arg in args:
+            if isinstance(arg, int):
+                if arg % 2 == 0:
+                    continue
+            return f"Please use only even numbers!"
+        return sum(args)
+
+    return wrapper
+
+@even_parameters
+def add(a, b):
+    return a + b
+print(add(2, 4))
+print(add("Peter", 1))
